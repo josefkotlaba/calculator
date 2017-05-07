3a48c44f5585b71b65aab68583834701f18861dd
josefkotlaba <josef.kotlaba@gmail.com>
2017-05-07 17:03:21 +0200

    Custom delimiters

--- calcString.py
+++ calcString.py
@@ -1,41 +1,61 @@
 class calcString:
     'Input string for the add function. Transforms input string and returns list of numbers'
-    
+    succesfulTests=0
+    tests=0
 
     def __init__(self, input):
         self.stringToConvert = input
         self.output = []
+
+    def test(self,description,output,str):       #Test function
+        calcString.tests+=1
+        self.stringToConvert=str
+        if (calcString.convertInput(self) == output) :
+            print("Test with "+description+" successful")
+            calcString.succesfulTests+=1
+            return
+        else:
+            print("Test with "+description+" failed!")
+            return
         
-    def testSuite(self):
-        succesfulTests=0
-        tests=0
+    def testSuite(self):    
+        calcString.test(self, "\"\"",[0],"")
+        calcString.test(self, "\"1\"",[1],"1")
+        calcString.test(self, "\"1\\n2\"",[1,2],"1\n2")
+        calcString.test(self, "\"1,\\n2\"",[1,0,2],"1,\n2")
+        calcString.test(self, "\"//[oo]\\n1,2\\n3o4\"",[0,0,0,1,2,3,4],"//[oo]\n1,2\n3oo4")
+        calcString.test(self, "\"//[[]\\n1,2\\n3[4\"",[0,0,0,0,1,2,3,4],"//[[]\n1,2\n3[4")
+        calcString.test(self, "empty string",[0],"")
+        calcString.test(self, "empty string",[0],"")
+        calcString.test(self, "empty string",[0],"")
 
-        tests+=1
-        self.stringToConvert=""
-        if (calcString.convertInput(self) == [0]): succesfulTests+=1; print("Test \"\" succesful")
-
-        tests+=1
-        self.stringToConvert="1"
-        if (calcString.convertInput(self) == [1]): succesfulTests+=1; print("Test \"1\" succesful")
-
-        tests+=1
-        self.stringToConvert="1\n2"
-        if (calcString.convertInput(self) == [1,2]): succesfulTests+=1; print("Test \"1\\n2\" succesful")
-
-        tests+=1
-        self.stringToConvert="1,\n2"
-        if (calcString.convertInput(self) == [1,0,2]): succesfulTests+=1; print("Test \"1,\\n2\" succesful")
-
-        if (tests==succesfulTests) : print("ALL CALCSTRING TESTS SUCCESFUL!")
+        if (calcString.tests==calcString.succesfulTests) : print("ALL CALCSTRING TESTS SUCCESFUL!")
         return;
 
+
+
     def convertInput(self):
-        self.stringToConvert=self.stringToConvert.replace("\n", ",")           #Replaces all commas with new lines
-        self.output=self.stringToConvert.split(',')     #Splits input string into list of strings delimited by a comma
+        #self.stringToConvert=self.stringToConvert.replace("\n", ",")           #Replaces all commas with new lines
+        convertString = self.stringToConvert
+        delimiters=["\n",",","//"]                            #List of available delimiters
+        #self.output=self.stringToConvert.split('\n')     #Splits input string into list of strings delimited by new lines
+
+        newlineDelimitedString = convertString.split('\n')
+        if newlineDelimitedString[0].startswith("//[") :    #If a first line starts with //
+             newDelimiter=newlineDelimitedString[0]
+             newDelimiter=newDelimiter[3:-1]                #get the part between brackets
+             delimiters.append(newDelimiter)                #and add it to delimiter list 
+
+        for index in range(len(delimiters)):
+            convertString = convertString.replace(delimiters[index],",") #replace all delimiters with commas
+
+        self.output = convertString.split(',')
+        
         for index in range(len(self.output)):
-            if (self.output[index] == ''): self.output[index]='0'    #If there is an emlty string in the list, it gets converted to 0
+            if (self.output[index] == ''): self.output[index]='0'    #If there is an empty string in the list, it gets converted to 0
 
         for index in range(len(self.output)):
+            if self.output[index] in ['[',']'] : self.output[index] = '0'
             self.output[index]=int(self.output[index])  #Converts strings to ints
 
         return self.output
--- calculator.py
+++ calculator.py
@@ -7,58 +7,56 @@
 #  Allow the add​ function to handle an unknown amount of numbers
 #  Allow the add​ function to handle new lines between numbers (instead of commas).
 #  Allow the add​ function to handle new lines between numbers (as well as commas).
-
-from calcString import calcString
-
-def testSuite():
+#  Support different delimiters
+#    - to change a delimiter, the beginning of the string will contain a separate line that looks like this:
+#      “//[[delimiter]]\n[numbers...]”
+#       for example “//[;]\n1;2” should return three where the default delimiter is ‘;’ .
+#    - the first line is optional. all existing scenarios should still be supported
+class Calculator:
+    'Main class for the calculator'
     succesfulTests=0
     tests=0
+from calcString import calcString
 
-    tests+=1
-    if (test("zero numbers",0,"") == "Success") : succesfulTests+=1
-
-    tests+=1
-    if (test("one number",1,"1") == "Success") : succesfulTests+=1
-
-    tests+=1
-    if (test("two numbers",7,"3\n4") == "Success") : succesfulTests+=1
-
-    tests+=1
-    if (test("two numbers",7,"3,4") == "Success") : succesfulTests+=1
-
-    tests+=1
-    if (test("double digits",31,"16\n15") == "Success") : succesfulTests+=1
-
-    tests+=1
-    if (test("double digits",31,"16,15") == "Success") : succesfulTests+=1
-
-    tests+=1
-    if (test("three numbers",6,"1\n2\n3") == "Success") : succesfulTests+=1
-
-    tests+=1
-    if (test("three numbers",6,"1\n2,3") == "Success") : succesfulTests+=1
-
-    tests+=1
-    if (test("multiple numbers",2165,"41,25\n33,156\n1,1888\n\n12,4\n5") == "Success") : succesfulTests+=1
-        
-    if (tests==succesfulTests) : print("ALL CALCULATOR TESTS SUCCESFUL!")
-    return;
-
-def test(description,output,str):
+def test(description,output,str):       #Test function
+    Calculator.tests+=1
     if (add(str) == output) :
         print("Test with "+description+" successful")
-        return("Success")
+        Calculator.succesfulTests+=1
+        return
     else:
         print("Test with "+description+" failed!")
-        return("Fail")
+        return
+
+def testSuite():                #run this to perform functional tests
+    test("zero numbers",0,"")
+    test("one number",1,"1")
+    test("two numbers, newline",7,"3\n4")
+    test("two numbers, comma",7,"3,4")
+    test("double digits",31,"16\n15")
+    test("double digits",31,"16,15")
+    test("three numbers",6,"1\n2\n3")
+    test("three numbers",6,"1\n2,3")
+    test("multiple numbers",2165,"41,25,33,156,1,1888,,12,4,5")
+    test("multiple numbers, newline delimiter",2165,"41\n25\n33\n156\n1\n1888\n\n12\n4\n5")
+    test("multiple numbers, multiple delimiters",2165,"41,25\n33,156\n1,1888\n\n12,4\n5")
+    test("multiple numbers, custom delimiter",2165,"//[a]\n41a25\n33,156\n1,1888\n\n12a4\n5")
+    test("multiple numbers, [ delimiter",2165,"//[[]\n41[25\n33,156\n1,1888\n\n12[4\n5")
+    test("multiple numbers, ] delimiter",2165,"//[]]\n41]25\n33,156\n1,1888\n\n12]4\n5")
+    test("multiple numbers, [] delimiter",2165,"//[[]]\n41[]25\n33,156\n1,1888\n\n12[]4\n5")
+        
+    if (Calculator.tests==Calculator.succesfulTests) : print("ALL CALCULATOR TESTS SUCCESFUL!")
+    return;
+
+
     
 
 def add(str):
     testString=calcString(str)          
-    result=testString.convertInput()    #Converts input to 
+    result=testString.convertInput()    #Converts input to list of ints
     output = 0
     for index in range(len(result)):
-      output=output+int(result[index])
+      output=output+int(result[index])  #Adds the ints
     return output;
 
 testSuite()
